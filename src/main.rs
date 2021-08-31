420efdcd2c9296ce220aff79886bba2f9ccc042e
Forrest Smith <forrestthewoods@gmail.com>
2021-08-31 00:24:48 -0700

    more progress

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use pdb::*;
 use std::fs::File;
-use std::io::Read;
 use std::path::Path;
 use structopt::StructOpt;
+use subprocess::*;
 
 #[derive(StructOpt, Debug)]
 #[structopt(
@@ -25,6 +25,9 @@ enum Op {
 
     #[structopt(name = "extract_all")]
     ExtractAll(ExtractAllOp),
+
+    #[structopt(name = "info")]
+    Info(InfoOp),
 }
 
 #[derive(Debug, StructOpt)]
@@ -48,6 +51,12 @@ struct ExtractAllOp {
     pdb: String,
 }
 
+#[derive(Debug, StructOpt)]
+struct InfoOp {
+    #[structopt(short, long, help = "Target PDB for specified operation")]
+    pdb: String,
+}
+
 /*
 fts_pdbsrc --embed --targetpdb foo
 fts_pdbsrc --extract --targetpdb
@@ -64,6 +73,7 @@ fn main() -> anyhow::Result<()> {
         Op::Embed(op) => embed(op)?,
         Op::ExtractOne(op) => extract_one(op)?,
         Op::ExtractAll(op) => extract_all(op)?,
+        Op::Info(op) => info(op)?,
     }
 
     Ok(())
@@ -73,8 +83,74 @@ fn embed(op: EmbedOp) -> anyhow::Result<(), anyhow::Error> {
     println!("Hello, world!");
 
     // Load PDB
-    let file = File::open(op.pdb)?;
-    let mut pdb = pdb::PDB::open(file)?;
+    let pdbfile = File::open(op.pdb)?;
+    let mut pdb = pdb::PDB::open(pdbfile)?;
+    let string_table = pdb.string_table()?;
+
+    // Iterate files
+    let mut filepaths : Vec<_> = Default::default();
+
+    let di = pdb.debug_information()?;
+    let mut modules = di.modules()?;
+    while let Some(module) = modules.next()? {
+        if let Some(module_info) = pdb.module_info(&module)? {
+            let line_program = module_info.line_program()?;
+
+            let mut file_iter = line_program.files();
+            while let Some(file) = file_iter.next()? {
+                let filename = string_table.get(file.name)?;
+
+                let filename_utf8 = std::str::from_utf8(filename.as_bytes())?;
+                let filepath = Path::new(filename_utf8);
+                filepaths.push(filepath);
+            }
+        }
+    }
+
+    // Iterate streams
+    let info = pdb.pdb_information()?;
+    let stream_names = info.stream_names()?;
+    stream_names
+        .iter()
+        .for_each(|stream_name| println!("Stream: [{}]", stream_name.name));
+
+    // Close PDB
+    drop(pdb);
+
+    // Now iterate files
+    for filepath in filepaths {
+        match std::fs::File::open(&filepath) {
+            Ok(_) => {
+                println!("File found, adding to pdb: [{:?}]", filepath);
+
+                let mut p = Popen::create(&["pdbstr", "-w", "-p:"], PopenConfig {
+                    stdout: Redirection::Pipe, ..Default::default()
+                })?;
+
+                let status = p.wait()?;
+            }
+            Err(_) => println!("File not found, skipping: [{:?}]", filepath),
+        }
+    }
+
+    println!("Goodbye cruel world!");
+
+    Ok(())
+}
+
+fn extract_one(op: ExtractOneOp) -> anyhow::Result<()> {
+    Ok(())
+}
+
+fn extract_all(op: ExtractAllOp) -> anyhow::Result<()> {
+    Ok(())
+}
+
+fn info(op: InfoOp) -> anyhow::Result<()> {
+
+    // Load PDB
+    let pdbfile = File::open(op.pdb)?;
+    let mut pdb = pdb::PDB::open(pdbfile)?;
     let string_table = pdb.string_table()?;
 
     // Iterate files
@@ -89,13 +165,12 @@ fn embed(op: EmbedOp) -> anyhow::Result<(), anyhow::Error> {
                 let filename = string_table.get(file.name)?;
 
                 let filename_utf8 = std::str::from_utf8(filename.as_bytes())?;
-                let filepath = Path::new(&filename_utf8);
+                let filepath = Path::new(filename_utf8);
 
-                match std::fs::File::open(&filepath) {
-                    Ok(_) => {
-                        println!("File (exists): [{}]", filename);
-                    }
-                    Err(_) => println!("File not found, skipping: [{}]", filename),
+                if std::fs::metadata(filepath).is_ok() {
+                    println!("File exists: [{:?}]", filepath);
+                } else {
+                    println!("File not found: [{:?}]", filepath);
                 }
             }
         }
@@ -108,16 +183,7 @@ fn embed(op: EmbedOp) -> anyhow::Result<(), anyhow::Error> {
         .iter()
         .for_each(|stream_name| println!("Stream: [{}]", stream_name.name));
 
-    println!("Goodbye cruel world!");
-
-    Ok(())
-}
-
-fn extract_one(op: ExtractOneOp) -> anyhow::Result<()> {
-    Ok(())
-}
-
-fn extract_all(op: ExtractAllOp) -> anyhow::Result<()> {
+        
     Ok(())
 }
 
