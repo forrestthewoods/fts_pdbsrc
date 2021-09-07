8006e648176960046c1d8d46ed58e4eb8437f548
Forrest Smith <forrestthewoods@gmail.com>
2021-09-06 20:01:08 -0700

    fix compile error

--- fts_pdbsrc_service/src/main.rs
+++ fts_pdbsrc_service/src/main.rs
@@ -1,3 +1,60 @@
-fn main() {
-    println!("Hello world");
+use std::ffi::OsString;
+use std::time::Duration;
+use windows_service::service::{
+    ServiceControl, ServiceControlAccept, ServiceExitCode, ServiceState, ServiceStatus,
+    ServiceType,
+};
+use windows_
+
+define_windows_service!(ffi_service_main, my_service_main);
+
+
+
+fn main() -> Result<(), windows_service::Error> {
+    // Register generated `ffi_service_main` with the system and start the service, blocking
+    // this thread until the service is stopped.
+    service_dispatcher::start("fts_pdbsrc_service", ffi_service_main)?;
+    Ok(())
+}
+
+fn my_service_main(arguments: Vec<OsString>) {
+    if let Err(_e) = run_service(arguments) {
+        // Handle errors in some way.
+    }
+}
+
+fn run_service(arguments: Vec<OsString>) -> windows_service::Result<()> {
+    let event_handler = move |control_event| -> ServiceControlHandlerResult {
+        match control_event {
+            ServiceControl::Stop | ServiceControl::Interrogate => {
+                ServiceControlHandlerResult::NoError
+            }
+            _ => ServiceControlHandlerResult::NotImplemented,
+        }
+    };
+
+    // Register system service event handler
+    let status_handle = service_control_handler::register("fts_pdbsrc_service", event_handler)?;
+
+    let next_status = ServiceStatus {
+        // Should match the one from system service registry
+        service_type: ServiceType::OWN_PROCESS,
+        // The new state
+        current_state: ServiceState::Running,
+        // Accept stop events when running
+        controls_accepted: ServiceControlAccept::STOP,
+        // Used to report an error when starting or stopping only, otherwise must be zero
+        exit_code: ServiceExitCode::Win32(0),
+        // Only used for pending states, otherwise must be zero
+        checkpoint: 0,
+        // Only used for pending states, otherwise must be zero
+        wait_hint: Duration::default(),
+    };
+
+    // Tell the system that the service is running now
+    status_handle.set_service_status(next_status)?;
+
+    // Do some work
+
+    Ok(())
 }
